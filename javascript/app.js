e7874998d234c022fbc9066f875b2ed02ed450ff
Natalieth11 <napebu@gmail.com>
2019-09-11 03:19:56 -0500

    interaccion simple primera parte

--- javascript/app.js
+++ javascript/app.js
@@ -5,6 +5,45 @@ btnMenu.addEventListener('click', function(){
     nav.classList.toggle('show');
 })
 
+/*-------------------interaccion simple------------------ */
+
+var img =document.getElementById("img-area")
+function sin(){
+    img.src='./imagenes/no-makeup.jpg';
+}
+
+
+function con(){
+    img.src='./imagenes/makeup.jpg';
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*var buttons = document.querySelectorAll('.manybtn__img');
+
+function handleClick1(event){
+    var name = event.target.getAttribute('src')
+    var parts = name.split('/');
+    var newName = './data/img' + parts[2];
+    var image = document.querySelector(".inter__img");/*imagen que voy a cambiar dependiendo del boon que unda 
+    image.setAttribute('src', newName);
+    console.log(newName);
+}
+
+for (var i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    button.addEventListener('click', handleClick1 );   
+}
+*/
 
 
 
